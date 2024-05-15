aab4e3e966be848116df10f4cbed068ec39d895a
Bektur Razzakov <brzzkv@gmail.com>
2024-05-15 15:24:31 +0600

    Initial containers

---
+++ src/containers/MailToAll/MailToAll.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MailToAll = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default MailToAll;
---
+++ src/containers/MailsList/MailsList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MailsList = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default MailsList;
