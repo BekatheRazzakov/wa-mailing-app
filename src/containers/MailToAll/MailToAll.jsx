69d609a18e557f5ea727e2580332ea6cfd511d47
Bektur Razzakov <brzzkv@gmail.com>
2024-05-21 17:48:41 +0600

    mailing on websocket

--- src/containers/MailToAll/MailToAll.jsx
+++ src/containers/MailToAll/MailToAll.jsx
@@ -1,53 +1,95 @@
-import React, {useState} from 'react';
-import {useNavigate} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axiosApi from '../../axiosApi';
 import DateTime from 'react-datetime';
+import * as XLSX from 'xlsx';
+
+const URL = "ws://localhost:8000/ws";
 
 const MailToAll = () => {
   const navigate = useNavigate();
+  const [ws, setWs] = useState(new WebSocket(URL));
   const [state, setState] = useState({
-    message: '',
-    excel_file: null,
+    message: '', abons: null,
   });
   const [selectedDate, setSelectedDate] = useState(null);
   const [loading, setLoading] = useState(false);
-
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [tagIndex, setTagIndex] = useState(false);
+  const [qr, setQr] = useState('');
+  
   const onChange = (e) => {
     const {name, value} = e.target;
     setState(prevState => ({
-      ...prevState,
-      [name]: value,
+      ...prevState, [name]: value,
     }));
+    setTagIndex(e.target.selectionStart - 1);
+    setShowSuggestions(e.nativeEvent.data === '@');
   };
-
+  
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
-  console.log('hello world');
-
+  
+  useEffect(() => {
+    ws.onopen = (e) => {
+      newFunction(e);
+      
+      function newFunction() {
+        console.log("WebSocket Connected");
+      }
+    };
+    
+    ws.onmessage = (e) => {
+      const message = JSON.parse(e.data);
+      if (message.type === 'qrCode' && message.qrCode) {
+        console.log(message.qrCode);
+        setQr(message.qrCode);
+      }
+      if (message.type === 'connecting') setLoading(true);
+      if (message.type === 'connected') setLoading(false);
+      if (message.type === 'error') setErrorMessage(message.message);
+      if (message.type !== 'error' && errorMessage) setErrorMessage('');
+      if (message.message === 'message sent') navigate('/all-mails');
+    };
+    
+    return () => {
+      ws.onclose = () => {
+        console.log("WebSocket Disconnected");
+        setWs(new WebSocket(URL));
+      };
+    };
+  }, [ws.onmessage, ws.onopen, ws.onclose, ws, URL]);
+  
+  const sendMessageToWS = (message) => {
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send(JSON.stringify(message));
+    } else {
+      console.log('WebSocket not open yet. Message not sent.');
+    }
+  };
+  
   const onSubmit = async (e) => {
     e.preventDefault();
     try {
       setLoading(true);
-      const formData = new FormData();
-
-      formData.append('message', state.message);
-      formData.append('excel_file', state.excel_file);
-      formData.append('scheduleDate', selectedDate);
-
-      await axiosApi.post('/mailing/send_to_all', formData);
+      await sendMessageToWS({
+        type: 'mailToAll', payload: state,
+      });
       setLoading(false);
-      navigate('/all-mails');
     } catch (e) {
       setLoading(false);
       console.log(e);
     }
   };
-
-  return (
-    <form className="login-form" onSubmit={onSubmit}>
-      <label htmlFor="exampleFormControlTextarea1" className="form-label">Сообщение</label>
+  
+  return (<form className="login-form" onSubmit={onSubmit}>
+    {
+      !!state.abons &&
+      <>
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">Сообщение</label>
+        <div style={{position: "relative"}}>
       <textarea
         className="form-control"
         id="exampleFormControlTextarea1"
@@ -55,27 +97,70 @@ const MailToAll = () => {
         name='message'
         value={state.message}
         onChange={onChange}
-      ></textarea>
-      <div className="mt-3">
-        <label htmlFor="formFile" className="form-label">Excel файл</label>
-        <input
-          className="form-control" type="file" id="formFile"
-          onChange={e => {
+      />
+          {
+            showSuggestions &&
+            <div className="list-group tag-suggestions" style={{position: "absolute", top: '100%', width: '100%'}}>
+              {
+                state.abons?.length && Object.keys(state.abons[0]).map((key) => (
+                  <span
+                    className="tag-suggestion list-group-item list-group-item-action"
+                    aria-current="true"
+                    onClick={() => {
+                      setState(prevState => ({
+                        ...prevState,
+                        message: state.message.slice(0, tagIndex + 1) + key + ' ' + state.message.slice(tagIndex + 1)
+                      }));
+                      setShowSuggestions(false);
+                    }}
+                  >{key}</span>
+                ))
+              }
+            </div>
+          }
+        </div>
+      </>
+    }
+    <div className="mt-3">
+      <label htmlFor="formFile" className="form-label">Excel файл</label>
+      <input
+        className="form-control" type="file" id="formFile"
+        onChange={e => {
+          const reader = new FileReader();
+          reader?.readAsBinaryString(e.target.files[0]);
+          
+          reader.onload = async (event) => {
+            const binaryStr = event.target.result;
+            const workbook = XLSX.read(binaryStr, {type: 'binary'});
+            
+            const sheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[sheetName];
+            
+            const json = XLSX.utils.sheet_to_json(worksheet);
+            
             setState(prevState => ({
-              ...prevState,
-              excel_file: e.target.files[0],
+              ...prevState, abons: json,
             }));
-          }}
-        />
-      </div>
-      <div className="mt-3">
-        <DateTime value={selectedDate} onChange={handleDateChange} />
-      </div>
-      <button type="submit" className="btn btn-primary mt-3"
-              disabled={loading || !state.excel_file || !state.message}>Отправить
-      </button>
-    </form>
-  );
+          };
+        }}
+      />
+    </div>
+    <div className="mt-3">
+      <DateTime value={selectedDate} onChange={handleDateChange}/>
+    </div>
+    <button type="submit" className="btn btn-primary mt-3"
+            disabled={loading || !state.abons || !state.message}>Отправить
+    </button>
+    <div style={{
+      display: 'flex', alignItems: 'center', flexDirection: 'column', marginTop: '20px', gap: '10px'
+    }}>
+      {!!errorMessage.length && <h6>{errorMessage}</h6>}
+      {qr && <>
+        <h6>Попробуйте отсканировать QR код, а затем отправить сообщение</h6>
+        <img src={qr} alt="QR code" width="200" height="200"/>
+      </>}
+    </div>
+  </form>);
 };
 
 export default MailToAll;
--- src/containers/MailToOne/MailToOne.jsx
+++ src/containers/MailToOne/MailToOne.jsx
@@ -1,62 +1,138 @@
-import React, {useState} from 'react';
-import {useNavigate} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import axiosApi from '../../axiosApi';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const URL = "ws://localhost:8000/ws";
 
 const MailToOne = () => {
-  const navigate = useNavigate();
-  const [state, setState] = useState({
-    message: '',
-    phone_number: '',
-  });
-  const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const [ws, setWs] = useState(new WebSocket(URL));
+    const [state, setState] = useState({
+        message: '',
+        phone_number: '',
+    });
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(false);
+    const [qr, setQr] = useState('');
 
-  const onChange = (e) => {
-    const {name, value} = e.target;
-    setState(prevState => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
+    useEffect(() => {
+        ws.onopen = (e) => {
+            newFunction(e);
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      setLoading(true);
-      await axiosApi.post('/mailing/send_to_one', state);
-      setLoading(false);
-      navigate('/all-mails');
-    } catch (e) {
-      setLoading(false);
-      console.log(e);
-    }
-  };
+            function newFunction() {
+                console.log("WebSocket Connected");
+            }
+        };
 
-  return (
-    <form className="login-form" onSubmit={onSubmit}>
-      <label htmlFor="inputPassword5" className="form-label">Номер телефона</label>
-      <input
-        type="text"
-        required
-        id="inputPassword5"
-        className="form-control"
-        aria-describedby="passwordHelpBlock"
-        name='phone_number'
-        value={state.phone_number}
-        onChange={onChange}
-      />
-      <label htmlFor="exampleFormControlTextarea1" className="form-label mt-2">Сообщение</label>
-      <textarea
-        className="form-control"
-        id="exampleFormControlTextarea1"
-        rows="3"
-        name='message'
-        value={state.message}
-        onChange={onChange}
-      ></textarea>
-      <button type="submit" className="btn btn-primary mt-3" disabled={loading || !state.phone_number || !state.message}>Отправить</button>
-    </form>
-  );
+        ws.onmessage = (e) => {
+            const message = JSON.parse(e.data);
+            if (message.type === 'qrCode' && message.qrCode) {
+                console.log(message.qrCode);
+                setQr(message.qrCode);
+            }
+            if (message.type === 'connecting') {
+                setLoading(true);
+                setQr('');
+                setErrorMessage('');
+            }
+            if (message.type === 'connected') setLoading(false);
+            if (message.type === 'error') setErrorMessage(message.message);
+            if (message.type !== 'error' && errorMessage) setErrorMessage('');
+            if (message.message === 'message sent') navigate('/all-mails');
+        };
+
+        return () => {
+            ws.onclose = () => {
+                console.log("WebSocket Disconnected");
+                setWs(new WebSocket(URL));
+            };
+        };
+    }, [ws.onmessage, ws.onopen, ws.onclose, ws, URL]);
+
+    const sendMessageToWS = (message) => {
+        if (ws.readyState === WebSocket.OPEN) {
+            ws.send(JSON.stringify(message));
+        } else {
+            console.log('WebSocket not open yet. Message not sent.');
+        }
+    };
+
+    const onChange = (e) => {
+        const {name, value} = e.target;
+        setState((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            setLoading(true);
+            await sendMessageToWS({
+                type: 'singleMailing',
+                payload: state,
+            });
+            setLoading(false);
+        } catch (e) {
+            setLoading(false);
+            console.log(e);
+        }
+    };
+
+    return (
+        <form className="login-form" onSubmit={onSubmit}>
+            <label htmlFor="inputPassword5" className="form-label">
+                Номер телефона
+            </label>
+            <input
+                type="text"
+                required
+                id="inputPassword5"
+                className="form-control"
+                aria-describedby="passwordHelpBlock"
+                name="phone_number"
+                value={state.phone_number}
+                onChange={onChange}
+            />
+            <label htmlFor="exampleFormControlTextarea1" className="form-label mt-2">
+                Сообщение
+            </label>
+            <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+                name="message"
+                value={state.message}
+                onChange={onChange}
+            ></textarea>
+            <button
+                type="submit"
+                className="btn btn-primary mt-3"
+                disabled={loading || !state.phone_number || !state.message}
+            >
+                Отправить
+            </button>
+            <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                flexDirection: 'column',
+                marginTop: '20px',
+                gap: '10px'
+            }}>
+                {
+                    !!errorMessage.length &&
+                    <h6>{errorMessage}</h6>
+                }
+                {
+                    qr &&
+                    <>
+                        <h6>Попробуйте отсканировать QR код, а затем отправить сообщение</h6>
+                        <img src={qr} alt="QR code" width="200" height="200"/>
+                    </>
+                }
+            </div>
+        </form>
+    );
 };
 
 export default MailToOne;
